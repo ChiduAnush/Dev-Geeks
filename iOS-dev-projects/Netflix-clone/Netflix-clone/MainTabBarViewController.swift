c8c1d0fedf74012f8750d581486fe00703c49737
ChiduAnush <chiduanush1@gmail.com>
2023-06-24 20:10:13 +0530

    add a system backgroud color to all tabs, and add icons and labels to each tab.

--- iOS-dev-projects/Netflix-clone/Netflix-clone/MainTabBarViewController.swift
+++ iOS-dev-projects/Netflix-clone/Netflix-clone/MainTabBarViewController.swift
@@ -23,6 +23,15 @@ class MainTabBarViewController: UITabBarController {
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
         vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = "Home"
+        vc2.title = "Coming soon"
+        vc3.title = "Top searches"
+        vc4.title = "Downloads"
+        
+        
+        
+        tabBar.tintColor = .label
 
 
         
