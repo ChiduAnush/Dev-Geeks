8b093d760c6cddd8050e933d3c3046f72182b2c4
ChiduAnush <chiduanush1@gmail.com>
2023-06-25 15:57:46 +0530

    make cells look a bit closer the the layout we want

--- iOS-dev-projects/Netflix-clone/Netflix-clone/Views/CollectionViewTableViewCell.swift
+++ iOS-dev-projects/Netflix-clone/Netflix-clone/Views/CollectionViewTableViewCell.swift
@@ -11,9 +11,24 @@ class CollectionViewTableViewCell: UITableViewCell {
 
     static let identifier = "CollectionViewTableViewCell"
     
+    
+    private let CollectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
+        contentView.addSubview(CollectionView)
+        
+        CollectionView.delegate = self
+        CollectionView.dataSource = self
     }
     
     
@@ -22,5 +37,25 @@ class CollectionViewTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError()
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        CollectionView.frame = contentView.bounds
+    }
+    
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
     
 }
